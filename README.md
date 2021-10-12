6d33508e71a962dcd1f7e7f0eec756b5c913f56d
zainbits <hello.zain11@gmail.com>
2021-10-12 21:00:12 +0530

    optimised css stylesheets

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ password: 12345
 
 # App URL
 http://ec2-13-232-206-75.ap-south-1.compute.amazonaws.com:3000/
+
+# Required
+.env file is required\
+create a .env file in root directory of this project\
+\
+.env file contains the following line:\
+REACT_APP_API_URL=http://BACKEND_IP/\
+where BACKEND_IP is the IP address of the backend\
--- src/App.js
+++ src/App.js
@@ -6,36 +6,34 @@ import { Dashboard } from "./Pages/Dashboard";
 import React from "react";
 
 export const SimpleContext = React.createContext("I am in App component");
-console.log(SimpleContext)
+console.log(SimpleContext);
 
 function App() {
   let contextValue = "I am in App component";
 
   return (
-    <div className="overflow-hidden">
-      <div className="row">
-        <div className="col-md-4 LeftPane">
-          <div className="login-lp">
-            <p>TODO - Web App{`\n`}Made in react</p>
-          </div>
-        </div>
-        <div className="col-md-8 block--right">
-          <SimpleContext.Provider value={contextValue}>
-            <Switch>
-              <Route exact path="/">
-                <LoginPage />
-              </Route>
-              <Route path="/getallusers">
-                <AllUsers />
-              </Route>
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
-            </Switch>
-          </SimpleContext.Provider>
+    <>
+      <div className="LeftPane">
+        <div className="login-lp">
+          <p>TODO - Web App{`\n`}Made in react</p>
         </div>
       </div>
-    </div>
+      <div className="block--right">
+        <SimpleContext.Provider value={contextValue}>
+          <Switch>
+            <Route exact path="/">
+              <LoginPage />
+            </Route>
+            <Route path="/getallusers">
+              <AllUsers />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </SimpleContext.Provider>
+      </div>
+    </>
   );
 }
 
